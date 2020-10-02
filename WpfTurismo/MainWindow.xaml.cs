4c7c78279d1d7fb73cafef020a7ae50a75199e8c
benjamin jimenez <Benjaminjimenez870@gmail.com>
2020-10-02 13:52:41 -0300

    commit

---
+++ WpfTurismo/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Converters;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Helpers;
+using LiveCharts.Wpf;
+
+namespace WpfTurismo
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SeriesCollection = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "2018",
+                    Values = new ChartValues<double> { 10, 50, 39, 50 }
+                }
+            };
+            SeriesCollection.Add(new ColumnSeries
+            {
+                Title = "2019",
+                Values = new ChartValues<double> { 11, 56, 42 }
+            });
+
+            //also adding values updates and animates the chart automatically
+            SeriesCollection[1].Values.Add(48d);
+
+            Labels = new[] { "Enero", "Febrero", "Marzo", "Abril" };
+            Formatter = value => value.ToString("N");
+
+            DataContext = this;
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> Formatter { get; set; }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = int.Parse(((Button)e.Source).Uid);
+
+            GridCursor.Margin = new Thickness(10 + (210 * index),0, 0, 0);
+
+            switch(index)
+            {
+                case 0:
+                    GridCursor.Background = Brushes.MistyRose;
+                    GridMain.Visibility = Visibility.Visible;
+                    GridDepa.Visibility = Visibility.Collapsed;
+                    GridArri.Visibility = Visibility.Collapsed;
+                    GridAdmin.Visibility = Visibility.Collapsed;
+                    GridFina.Visibility = Visibility.Collapsed;
+                    break;
+                case 1:
+                    GridCursor.Background = Brushes.YellowGreen;
+                    GridMain.Visibility = Visibility.Collapsed;
+                    GridDepa.Visibility = Visibility.Visible;
+                    GridArri.Visibility = Visibility.Collapsed;
+                    GridAdmin.Visibility = Visibility.Collapsed;
+                    GridFina.Visibility = Visibility.Collapsed;
+                    break;
+                case 2:
+                    GridCursor.Background = Brushes.MistyRose;
+                    GridMain.Visibility = Visibility.Collapsed;
+                    GridDepa.Visibility = Visibility.Collapsed;
+                    GridArri.Visibility = Visibility.Visible;
+                    GridAdmin.Visibility = Visibility.Collapsed;
+                    GridFina.Visibility = Visibility.Collapsed;
+                    break;
+                case 3:
+                    GridCursor.Background = Brushes.Orange;
+                    GridMain.Visibility = Visibility.Collapsed;
+                    GridDepa.Visibility = Visibility.Collapsed;
+                    GridArri.Visibility = Visibility.Collapsed;
+                    GridAdmin.Visibility = Visibility.Visible;
+                    GridFina.Visibility = Visibility.Collapsed;
+                    break;
+                case 4:
+                    GridCursor.Background = Brushes.Red;
+                    GridMain.Visibility = Visibility.Collapsed;
+                    GridDepa.Visibility = Visibility.Collapsed;
+                    GridArri.Visibility = Visibility.Collapsed;
+                    GridAdmin.Visibility = Visibility.Collapsed;
+                    GridFina.Visibility = Visibility.Visible;
+                    break;
+            }
+
+        }
+
+        private void Button_Click_Cerrar(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
